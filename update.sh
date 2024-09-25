a658f768a60a5de0831e80a3b5b208130d305844
Dan <167598001+Saaapiker@users.noreply.github.com>
2024-09-25 19:53:42 +0300

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Параметры
+GITHUB_REPO="https://raw.githubusercontent.com/Saaapiker/legorob/refs/heads/main/v1.py?token=GHSAT0AAAAAACYBCLPIJKRNYCW3X7GZ75F6ZXUH7DA"  # URL к файлу на GitHub
+LOCAL_FILE="/Legorob"  # Локальный путь к Python-скрипту
+PROCESS_NAME="v1.py"  # Имя Python-скрипта
+
+# Функция для проверки обновления файла на GitHub
+check_for_update() {
+  # Загружаем последний файл с GitHub
+  curl -s $GITHUB_REPO -o /tmp/v1.py
+
+  # Проверяем изменения
+  if ! cmp -s /tmp/v1.py $LOCAL_FILE; then
+    echo "Скрипт обновлен. Скачиваем новую версию."
+    cp /tmp/v1.py $LOCAL_FILE
+    restart_process
+  else
+    echo "Скрипт не обновлен."
+  fi
+
+  # Удаляем временный файл
+  rm /tmp/v1.py
+}
+
+# Функция для перезапуска Python-скрипта
+restart_process() {
+  echo "Перезапуск Python-скрипта $PROCESS_NAME..."
+  
+  # Останавливаем запущенный Python-скрипт (если он существует)
+  pkill -f $LOCAL_FILE
+
+  # Запускаем скрипт заново
+  nohup python3 $LOCAL_FILE &
+
+  echo "$PROCESS_NAME перезапущен."
+}
+
+# Основной цикл проверки обновлений
+while true; do
+  check_for_update
+  # Проверять обновления каждые 60 секунд
+  sleep 60
+done
