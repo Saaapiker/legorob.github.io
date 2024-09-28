b910ea0430eba6cba5eaad747ec8d26658e004dd
Dan <167598001+Saaapiker@users.noreply.github.com>
2024-09-28 20:00:50 +0300

    Update v1.py

--- v1.py
+++ v1.py
@@ -3,7 +3,7 @@ variable = 0
 while True:
   print("BOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOORING")
   variable += 1
-  if variable = 100:
+  if variable == 100:
     exit()
 
 
