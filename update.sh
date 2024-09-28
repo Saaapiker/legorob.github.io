454d9a6c83646c75defc89cc09d37b3d17e295c3
Dan <167598001+Saaapiker@users.noreply.github.com>
2024-09-28 19:08:01 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 
 # Параметры
-GITHUB_REPO="curl -s https://raw.githubusercontent.com/Saaapiker/legorob/refs/heads/main/v1.py?token=GHSAT0AAAAAACYBCLPJGLSMQYNEHEMQMHKWZXUI2JQ"  # URL к файлу на GitHub
-LOCAL_FILE="/legorob/github/v1.py"  # Локальный путь к Python-скрипту
+LOCAL_FILE="/legorob/v1.py"  # Локальный путь к Python-скрипту
 PROCESS_NAME="v1.py"  # Имя Python-скрипта
 
 # Функция для проверки обновления файла на GitHub
 check_for_update() {
   # Загружаем последний файл с GitHub
-  curl -s $GITHUB_REPO -o /tmp/v1.py
+  git pull
 
   # Проверяем изменения
-  if ! cmp -s /tmp/v1.py $LOCAL_FILE; then
+  if ! cmp -s v1.py $LOCAL_FILE; then
     echo "Скрипт обновлен. Скачиваем новую версию."
-    cp /tmp/v1.py $LOCAL_FILE
+    cp v1.py $LOCAL_FILE
     restart_process
   else
     echo "Скрипт не обновлен."
   fi
-
-  # Удаляем временный файл
-  rm /tmp/v1.py
 }
 
 # Функция для перезапуска Python-скрипта
